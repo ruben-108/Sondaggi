690fabb2d7d9c1c2bd0bee81bddf58fda489e9db
Ruben <RubenDeacon@protonmail.com>
2025-03-12 17:30:14 +0100

    Added cookies

--- sondaggi/models.py
+++ sondaggi/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Question(models.Model):
 	testo_domanda = models.CharField(max_length=200)
 	pub_data = models.DateTimeField(name='data pubblicazione')
-	votes = models.ManyToManyField('Vote', related_name='voted_questions', blank=True)
 
 	def __str__(self):
 		return self.testo_domanda
@@ -16,11 +15,3 @@ class Choice(models.Model):
 
 	def __str__(self):
 		return self.testo_opzione
-
-
-class Vote(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    user_ip = models.GenericIPAddressField()
-
-    def __str__(self):
-        return f"IP Utente: {self.user_ip}, Domanda: {self.question}"
--- sondaggi/views.py
+++ sondaggi/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
-from django.http import HttpResponseForbidden
 from django.db.models import F
 from django.urls import reverse
-from .models import Question, Choice, Vote
+import datetime
+from .models import Question, Choice
 
 def index(request):
 	lista_domande = Question.objects.order_by('data pubblicazione')
@@ -19,38 +19,37 @@ def risultati(request, question_id):
     lista_domande_opzioni = {'question_id': question_id, 'question': question}
     return render(request, 'sondaggi/risultati.html', lista_domande_opzioni)
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+def mostra_errori(request, question, messaggio_errore):
+    """Funzione di utilità per gestire la visualizzazione degli errori e il rendering del template"""
+    choices = Choice.objects.filter(question=question)
+    return render(request, "sondaggi/dettagli.html", {
+        'question_id': question.id,
+        'question': question,
+        'choices': choices,
+        'messaggio_errore': messaggio_errore
+    })
 
 def voti(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    
-    user_ip = get_client_ip(request)
-    
-    if question.votes.filter(user_ip=user_ip).exists():
+
+    cookie_name = f'voted_{question_id}'
+    if request.COOKIES.get(cookie_name):
         messaggio_errore = "Hai già votato in questo sondaggio!"
-        choices = Choice.objects.filter(question=question_id)
-        lista_domande_opzioni = {'question_id': question_id, 'question': question, 'choices': choices, 'messaggio_errore': messaggio_errore}
-        return render(request, "sondaggi/dettagli.html", lista_domande_opzioni)
-    
+        return mostra_errori(request, question, messaggio_errore)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Ridisplay della domanda di voto
-        choices = Choice.objects.filter(question=question_id)
-        lista_domande_opzioni = {'question_id': question_id, 'question': question, 'choices': choices}
-        return render(request, "sondaggi/dettagli.html", lista_domande_opzioni)
+        messaggio_errore = "Opzione non valida!"
+        return mostra_errori(request, question, messaggio_errore)
     
     selected_choice.voti = F("voti") + 1
     selected_choice.save()
     
-    # Salva l'IP dell'utente come già votante
-    question.votes.create(user_ip=user_ip, question=question)
+    # Crea un cookie per marcare l'utente come votante
+    response = HttpResponseRedirect(reverse("sondaggi:risultati", args=(question_id,)))
+    
+    # Aggiungi il cookie con il nome `voted_<question_id>` e un valore unico basato sulla domanda
+    response.set_cookie(cookie_name, 'true', max_age=datetime.timedelta(days=365))  # Il cookie durerà per un anno
 
-    # Redirect alla pagina dei risultati
-    return HttpResponseRedirect(reverse("sondaggi:risultati", args=(question_id, )))
+    return response
