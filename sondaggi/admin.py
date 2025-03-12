e751565a6326786b946141b365ffb33ca0ae51a7
Ruben <RubenDeacon@protonmail.com>
2025-03-12 16:07:20 +0100

    Updated Admin Interface

--- sondaggi/admin.py
+++ sondaggi/admin.py
@@ -1,16 +1,40 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question, Choice, Vote
 
 class ChoiceInline(admin.TabularInline):
 	model = Choice
 	extra = 2
 
-class DomandaAdmin(admin.ModelAdmin):
+class QuestionAdmin(admin.ModelAdmin):
 	fieldset = [
 		(None, {'fields': ['testo_domanda']}),
 		("Informazioni data", {'fields': ['pub_data'], }),
 	]
+	exclude = ('votes',)
 	inlines = [ChoiceInline]
 	list_display = ['testo_domanda', 'data pubblicazione']
 
-admin.site.register(Question, DomandaAdmin)
+class VoteAdmin(admin.ModelAdmin):
+    # Mostra i campi da visualizzare nell'admin
+    list_display = ('question', 'user_ip', 'get_question_text')
+    
+    # Aggiungi un campo aggiuntivo che mostri il testo della domanda
+    def get_question_text(self, obj):
+        return obj.question.testo_domanda
+    get_question_text.short_description = 'Testo della domanda'
+    
+    # Rendi i campi solo in lettura
+    readonly_fields = ('question', 'user_ip')
+
+    # Disabilita la possibilità di aggiungere, modificare o eliminare oggetti
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False 
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Vote)
--- sondaggi/models.py
+++ sondaggi/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Question(models.Model):
 	testo_domanda = models.CharField(max_length=200)
 	pub_data = models.DateTimeField(name='data pubblicazione')
+	votes = models.ManyToManyField('Vote', related_name='voted_questions', blank=True)
 
 	def __str__(self):
 		return self.testo_domanda
@@ -22,4 +23,4 @@ class Vote(models.Model):
     user_ip = models.GenericIPAddressField()
 
     def __str__(self):
-        return f"Voto di {self.user_ip} per la domanda {self.question}"
+        return f"IP Utente: {self.user_ip}, Domanda: {self.question}"
